7043623c98cba33dbb07e1b655d71c559d2d9919
jirathip-k <jkun0008@student.monash.edu>
2023-08-30 10:26:21 +1000

    format code and test request (only GET works and cannt send data to the server)

--- app/(tabs)/result.tsx
+++ app/(tabs)/result.tsx
@@ -1,25 +1,57 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from '../../store';
-import useLogin from '../../hooks/useLogin';
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../store";
+import useLogin from "../../hooks/useLogin";
+import useFetch from "../../hooks/useFetch";
 
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+import MapView, { Marker } from "react-native-maps";
 
 export default function TabTwoScreen() {
   const count = useSelector((state: RootState) => state.counter.count);
   const dispatch = useDispatch();
 
-  const { isLoading, token, error } = useSelector((state: RootState) => state.login)
-  useLogin('admin','admin')
-  console.log(isLoading, token, error)
+  const { isLoadingLogin, token, errorLogin } = useSelector(
+    (state: RootState) => state.login
+  );
+  useLogin("admin", "admin");
+
+  const { isLoadingFetch, dataFetched, errorFetch } = useSelector(
+    (state: RootState) => state.fetchData
+  );
+  useFetch("user/generate", "GET", token, {
+    username: "admin1",
+  });
+  console.log(dataFetched);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two {count}</Text>
-      <Text onPress={() => dispatch({ type: 'counter/increment' })}>Increment</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text onPress={() => dispatch({ type: "counter/increment" })}>
+        Increment
+      </Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+          title="My Marker"
+          description="This is my marker"
+        />
+      </MapView>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
@@ -28,16 +60,19 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
