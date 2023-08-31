a3c6875e9ebd9ecc3b97b2c412cb6a36423c8f3c
cam <cwan0148@student.monash.edu>
2023-08-31 13:53:38 +1000

    add svg support

--- app/common/language.tsx
+++ app/common/language.tsx
@@ -1,15 +1,15 @@
-import { View } from "react-native";
+import { View, Image } from "react-native";
 import { useTheme } from "react-native-paper";
+import TranslateIcon from "../../assets/images/icons/translate.svg";
 
-export default function LocaleScreen() {
+export default function LanguageScreen() {
   const theme = useTheme();
   return (
     <View style={{ 
       backgroundColor: theme.colors.primaryContainer, 
       flex: 1, 
       flexDirection: 'column'}}>
-        
-
+        <TranslateIcon width={200} height={200} fill={theme.colors.onPrimaryContainer} />
     </View>
   );
 }
