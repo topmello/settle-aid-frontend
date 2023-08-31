ad94846b4a31a47cad246a6f678daa1aa9cc2995
GordonMonash <gleu0001@student.monash.edu>
2023-08-31 23:14:40 +1000

    Learn Screen Tips Created with Basic Styles

--- app/(tabs)/learn.tsx
+++ app/(tabs)/learn.tsx
@@ -1,10 +1,70 @@
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import tips from "../../tips/tips.json";
 
 export default function LearnScreen() {
-  return (
-    <View>
-      <Text variant="bodyLarge">Learn Screen</Text>
+  const holder = tips.tips.map((activity, activityIndex) => (
+    <View key={`activity_${activityIndex}`} style={styles.titleActivityContainer}>
+      <Text style={styles.titleActivity}>
+        {activity.title} {'\n\n'}
+        {activity.tips.map((item, index) => (
+          <View key={`item_${index}`} style={styles.titleContentContainer}>
+            <Text style={styles.titleContent}>
+              {item.mode} {"\n\n"}
+              {item.type.map((typeItem, typeIndex) => (
+                <View key={`typeItem_${typeIndex}`} style={styles.contentContainer}>
+                  <Text>
+                    {typeIndex + 1} {". "}
+                    {typeItem.description + '\n'}
+                    {typeItem.content + '\n\n\n'}
+                  </Text>
+                </View>
+              ))}
+            </Text>
+          </View>
+        ))}
+      </Text>
     </View>
+  ));
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Cultural Tips</Text>
+      <ScrollView>{holder}</ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    marginTop: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  titleActivity: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  titleContent: {
+    fontSize: 15,
+    textAlign: 'left',
+  },
+  content: {},
+  titleContentContainer: {
+    padding: 10,
+  },
+  titleActivityContainer: {
+    paddingHorizontal: 20,
+  },
+  contentContainer: {
+    paddingHorizontal: 20,
+  }
+});
